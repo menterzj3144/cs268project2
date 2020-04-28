cc0dd78f5581658b968e9d3df57e41c7f9783370
ttkhband <ttkhband@gmail.com>
2020-04-28 14:53:24 -0500

    Oh man

--- src/piano.js
+++ src/piano.js
@@ -15,6 +15,18 @@ export function Piano() {
             <button className="white-key">A</button>
             <button className="black-key" id="as">A#</button>
             <button className="white-key">B</button>
+            <button className="white-key">C</button>
+            <button className="black-key" id="cs2">C#</button>
+            <button className="white-key">D</button>
+            <button className="black-key" id="ds2">D#</button>
+            <button className="white-key">E</button>
+            <button className="white-key">F</button>
+            <button className="black-key" id="fs2">F#</button>
+            <button className="white-key">G</button>
+            <button className="black-key" id="gs2">G#</button>
+            <button className="white-key">A</button>
+            <button className="black-key" id="as2">A#</button>
+            <button className="white-key">B</button>
         </div>
     );
 }
--- src/staff.js
+++ src/staff.js
@@ -3,7 +3,6 @@ import React from 'react';
 export function Staff() {
     return (
         <div className="staff">
-            
         </div>
     )
 }
