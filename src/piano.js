190f12af16dda148bc9658894ff0b77d3edc430d
ttkhband <ttkhband@gmail.com>
2020-04-29 19:24:38 -0500

    Piano sounds

--- src/piano.js
+++ src/piano.js
@@ -1,32 +1,81 @@
 import React from 'react';
 
 export function Piano() {
+    const playC = () => {
+        const c = new Audio("/audio/c.wav");
+        c.play();
+    }
+
+    const playCs = () => {
+        const cs = new Audio("/audio/cs.wav");
+        cs.play();
+    }
+
+    const playD = () => {
+        const d = new Audio("/audio/d.wav");
+        d.play();
+    }
+
+    const playDs = () => {
+        const ds = new Audio("/audio/ds.wav");
+        ds.play();
+    }
+
+    const playE = () => {
+        const e = new Audio("/audio/e.wav");
+        e.play();
+    }
+
+    const playF = () => {
+        const f = new Audio("/audio/f.wav");
+        f.play();
+    }
+
+    const playFs = () => {
+        const fs = new Audio("/audio/fs.wav");
+        fs.play();
+    }
+
+    const playG = () => {
+        const g = new Audio("/audio/g.wav");
+        g.play();
+    }
+
+    const playGs = () => {
+        const gs = new Audio("/audio/gs.wav");
+        gs.play();
+    }
+
+    const playA = () => {
+        const a = new Audio("/audio/a.wav");
+        a.play();
+    }
+
+    const playAs = () => {
+        const as = new Audio("/audio/as.wav");
+        as.play();
+    }
+
+    const playB = () => {
+        const b = new Audio("/audio/b.wav");
+        b.play();
+    }
+
     return (
         <div className="piano">
-            <button className="white-key">C</button>
-            <button className="black-key" id="cs">C#</button>
-            <button className="white-key">D</button>
-            <button className="black-key" id="ds">D#</button>
-            <button className="white-key">E</button>
-            <button className="white-key">F</button>
-            <button className="black-key" id="fs">F#</button>
-            <button className="white-key">G</button>
-            <button className="black-key" id="gs">G#</button>
-            <button className="white-key">A</button>
-            <button className="black-key" id="as">A#</button>
-            <button className="white-key">B</button>
-            <button className="white-key">C</button>
-            <button className="black-key" id="cs2">C#</button>
-            <button className="white-key">D</button>
-            <button className="black-key" id="ds2">D#</button>
-            <button className="white-key">E</button>
-            <button className="white-key">F</button>
-            <button className="black-key" id="fs2">F#</button>
-            <button className="white-key">G</button>
-            <button className="black-key" id="gs2">G#</button>
-            <button className="white-key">A</button>
-            <button className="black-key" id="as2">A#</button>
-            <button className="white-key">B</button>
+            <button className="white-key" onClick={playC}>C</button>
+            <button className="black-key" id="cs" onClick={playCs}>C#</button>
+            <button className="white-key" onClick={playD}>D</button>
+            <button className="black-key" id="ds" onClick={playDs}>D#</button>
+            <button className="white-key" onClick={playE}>E</button>
+            <button className="white-key" onClick={playF}>F</button>
+            <button className="black-key" id="fs" onClick={playFs}>F#</button>
+            <button className="white-key" onClick={playG}>G</button>
+            <button className="black-key" id="gs" onClick={playGs}>G#</button>
+            <button className="white-key" onClick={playA}>A</button>
+            <button className="black-key" id="as" onClick={playAs}>A#</button>
+            <button className="white-key" onClick={playB}>B</button>
+
         </div>
     );
 }
