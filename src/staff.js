772425756eb5ac2608dc7f042adacf25c43ff409
ttkhband <ttkhband@gmail.com>
2020-05-14 16:36:54 -0500

    Better

--- src/staff.js
+++ src/staff.js
@@ -25,54 +25,9 @@ export function Staff() {
     };
 
     useEffect(() => {
-        // var numStaves = window.innerWidth / 400;
-        // var x = numStaves % 1;
-        // numStaves = numStaves - x;
-        // x = x * 400;
-        // x = x / 2;
-        // var xposition = x;
-        // var yposition = -10;
-
         var vexStaves = convertStaves(staves);
         var vexNotes = convertNotes(completedBars, barInProgress);
-        
-        // WorkspaceInformation = {
-        //     canvas: document.getElementById("staff"),
-        //     canvasWidth: window.innerWidth,
-        //     canvasHeight: 100
-        // };
-        // document.getElementById("staff").style.visibility = "visible";
-    
-        // renderer = new VF.Renderer(
-        //     WorkspaceInformation.canvas,
-        //     VF.Renderer.Backends.CANVAS
-        // );
-    
-        // renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
-    
-        // context = renderer.getContext();
-    
-        // context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
-        // const canvas = document.getElementById("staff");
-        // context = canvas.getContext("2d");
-        // context.clearRect(0, 0, canvas.clientWidth, canvas.height);
-
-        // var i;
-        // for (i = 0; i < vexStaves.length; i++) {
-        //     console.log(vexStaves);
-        //     if (xposition + 400 > window.innerWidth) {
-        //         xposition = x;
-        //         yposition += 100;
-        //         WorkspaceInformation.canvasHeight += 100;
-        //         renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
-        //     }
-        //     vexStaves[i].setContext(context).setX(xposition).setY(yposition).setWidth(400).draw();
-        //     xposition += 400;
-        //     if (vexNotes[i] != null) {
-        //         VF.Formatter.FormatAndDraw(vexStaves[i].context, vexStaves[i], vexNotes[i]);
-        //     }
-        // }
         if (staves[0] != null) {
             const canvas = document.getElementById("staff");
             const context = canvas.getContext('2d');
